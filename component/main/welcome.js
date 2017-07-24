5c4c4aea33ed60356f9408bbb4c20952ae399645
jaeng <ljaeng@163.com>
2017-07-24 21:34:27 +0800

    设置Android的启动页

---
+++ component/main/welcome.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jaeng on 2017/7/24.
+ */
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+} from 'react-native';
+
